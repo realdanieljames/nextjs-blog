2bbf752821f90289338a3b678d994372437f088b
DANIEL JAMES <daniel.james@codeimmersives.com>
2021-06-14 14:56:55 -0700

    added date commponent to handle date in more readable format and added css to Post

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,48 +1,45 @@
-
-import Layout from "../../components/layout"
-// The post page is now using the getPostData function in getStaticProps 
+import Head from "next/head";
+import Layout from "../../components/layout";
+// The post page is now using the getPostData function in getStaticProps
 // to get the post data and return it as props.
-import {getAllPostIds, getPostData} from "../../lib/posts"
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
 
-
-export async function getStaticProps({params}){
-    const postData = await getPostData(params.id)
-    return {
-        props:{
-            postData
-        }
-    }
-
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
 }
 
+export async function getStaticPaths() {
+  // paths contains the array of known paths returned by getAllPostIds()
+  //which include the params defined by pages/posts/[id].js
+  const paths = getAllPostIds();
 
-
-export async function getStaticPaths(){
-    // paths contains the array of known paths returned by getAllPostIds()
-    //which include the params defined by pages/posts/[id].js
-    const paths = getAllPostIds()
-        
-    return {
-        paths, 
-        fallback: false
-    }
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
-
-
-export default function Post({postData}){
-    return (
-
-        <Layout>
-            {postData.title}
-            <br />
-            {postData.id}
-            <br />
-            {postData.date}
-            <br />
-            {/* render contentHtml using dangerouslySetInnerHTML: */}
-            <div dangerouslySetInnerHTML={{__html: postData.contentHtml}}/>
-        </Layout>
-    )
-    
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        {/* render contentHtml using dangerouslySetInnerHTML: */}
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
 }
