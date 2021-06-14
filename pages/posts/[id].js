e1e0c96b8602c04545db6a4b597f4271aa8f127d
DANIEL JAMES <daniel.james@codeimmersives.com>
2021-06-14 13:59:37 -0700

    update the Post component to use postData. In pages/posts/[id].js

---
+++ pages/posts/[id].js
@@ -0,0 +1,43 @@
+import { getStaticProps } from ".."
+import Layout from "../../components/layout"
+// The post page is now using the getPostData function in getStaticProps 
+// to get the post data and return it as props.
+import {getAllPostIds, getPostData} from "../../lib/posts"
+
+
+export async function getStaticProps({params}){
+    const postData = getPostData(params.id)
+    return {
+        props:{
+            postData
+        }
+    }
+
+}
+
+
+
+export async function getStaticPaths(){
+    // paths contains the array of known paths returned by getAllPostIds()
+    //which include the params defined by pages/posts/[id].js
+    const paths = getAllPostIds()
+        
+    return {
+        paths, 
+        fallback: false
+    }
+}
+
+
+
+export default function Post({postData}){
+    return (
+
+        <Layout>
+            {postData.title}
+            <br />
+            {postData.date}
+        </Layout>
+    )
+    
+}
