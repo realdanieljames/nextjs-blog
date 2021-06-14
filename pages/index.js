02229b8f81b4f0c837453e850f22a4ce6f6a3702
DANIEL JAMES <daniel.james@codeimmersives.com>
2021-06-14 11:59:05 -0700

    updated the Home component to add another <section> tag with the data below to display the blog posts

--- pages/index.js
+++ pages/index.js
@@ -2,10 +2,26 @@
 import Head from 'next/head'
 import Layout,{ siteTitle } from '../components/layout'
 import utilStyles from "../styles/utils.module.css"
+import { getSortedPostsData } from '../lib/posts'
 
 
 
-export default function Home() {
+
+// By returning allPostsData inside the props object in getStaticProps,
+//  the blog posts will be passed to the Home component as a prop
+export async function getStaticProps(){
+  const allPostsData = getSortedPostsData()
+  return {
+    props:{
+      allPostsData
+    }
+  }
+}
+
+
+
+// Now you can access the blog posts like so:
+export default function Home({allPostsData}) {
   return (
     <Layout home>
       <Head>
@@ -18,6 +34,22 @@ export default function Home() {
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
+
+    <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+      <h2 className={utilStyles.headingLg}>Blog</h2>
+      <ul className={utilStyles.list}>
+        {allPostsData.map(({id,date, title})=>(
+          <li className={utilStyles.listItem} key={id}>
+            {title}
+            <br />
+            {id}
+            <br />
+            {date}
+          </li>
+        ))}
+      </ul>
+    </section>
+
     </Layout>
   )
 
